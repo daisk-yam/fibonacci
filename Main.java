16dadd37c7ea941f0e8890e850be6a8910bcd103
daisk-yam <daisukes@hotmail.com.br>
2018-10-02 15:36:57 -0300

    primeiro commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.diin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(4));
+    }
+    public static int fibonacci(int n){
+        if(n < 2)  return n;
+        return fibonacci(n - 1) + fibonacci( n - 2);
+    }
+}
